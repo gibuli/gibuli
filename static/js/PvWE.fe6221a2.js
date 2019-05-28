a6f04f05ff9c5b8c155bea9fb6201368fbe19e48
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-28 11:58:07 +0000

    Deploy gibuli/gibuli to github.com/gibuli/gibuli.git:gh-pages

---
+++ static/js/PvWE.fe6221a2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["PvWE"],{PvWE:function(e,t,n){"use strict";n.r(t);var i=n("KHd+"),l=Object(i.a)({},function(){var e=this.$createElement,t=this._self._c||e;return t("div",{staticStyle:{padding:"30px"}},[t("router-view")],1)},[],!1,null,null,null);l.options.__file="index.vue";t.default=l.exports}}]);
