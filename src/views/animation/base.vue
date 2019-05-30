4bfbbbe60a048aa246f8a6a726e22aa335efea61
jinchen-s <jinchen-s@360.cn>
2019-05-30 15:05:07 +0800

    'di'

---
+++ src/views/animation/base.vue
@@ -0,0 +1,284 @@
+<template >
+  <div style="padding:30px;">
+    <!--priview-->
+    <div>
+      <strong>animation-fill-mode（默认：backwards）</strong>
+      <div class="demo_ani demo_1">
+        <div></div>
+      </div>
+    </div>
+    <!--priview-->
+    <!--code-->
+    <div style="width:800px;">
+      <script
+        type="text/plain"
+        class="language-markup"
+      >
+        <div class="demo_ani demo_1">
+            <div></div>
+        </div>
+      </script>
+
+      <pre><code class="language-scss">
+        @keyframes move{
+		  0%{left:0}
+		  100%{left:600px;}
+		}
+
+		.demo_ani{
+			position: relative;
+			height: 120px;
+		}
+
+		.demo_ani div{
+			width: 100px;
+			height: 100px;
+			border:1px solid #000;
+  			background:red;
+  			position: absolute;
+			top:0;
+			left:0;
+		}
+
+		.demo_1 div.move{
+			animation:move 2s;
+		}
+      </code></pre>
+
+    </div>
+    <!--code-->
+
+    <!--priview-->
+    <div>
+      <strong>animation-fill-mode（forwards）</strong>
+      <div class="demo_ani demo_2">
+        <div></div>
+      </div>
+
+    </div>
+    <!--priview-->
+    <!--code-->
+    <div style="width:800px;">
+      <script
+        type="text/plain"
+        class="language-markup"
+      >
+        <div class="demo_ani demo_2">
+            <div></div>
+        </div>
+      </script>
+
+      <pre><code class="language-scss">
+        @keyframes move{
+		  0%{left:0}
+		  100%{left:600px;}
+		}
+
+		.demo_ani{
+			position: relative;
+			height: 120px;
+		}
+
+		.demo_ani div{
+			width: 100px;
+			height: 100px;
+			border:1px solid #000;
+  			background:red;
+  			position: absolute;
+			top:0;
+			left:0;
+		}
+
+		.demo_2 div.move{
+			animation:move 2s forwards;
+		}
+      </code></pre>
+
+    </div>
+    <!--code-->
+
+    <!--priview-->
+    <div>
+      <strong>animation-direction（默认：normal）</strong>
+      <div class="demo_ani demo_3">
+        <div></div>
+      </div>
+    </div>
+    <!--priview-->
+    <!--code-->
+    <div style="width:800px;">
+      <script
+        type="text/plain"
+        class="language-markup"
+      >
+        <div class="demo_ani demo_3">
+            <div></div>
+        </div>
+      </script>
+
+      <pre><code class="language-scss">
+        @keyframes move{
+		  0%{left:0}
+		  100%{left:600px;}
+		}
+
+		.demo_ani{
+			position: relative;
+			height: 120px;
+		}
+
+		.demo_ani div{
+			width: 100px;
+			height: 100px;
+			border:1px solid #000;
+  			background:red;
+  			position: absolute;
+			top:0;
+			left:0;
+		}
+
+		.demo_3 div.move{
+			animation:move 2s forwards 6;/*0-600px 6次*/
+		}
+      </code></pre>
+
+    </div>
+    <!--code-->
+
+    <!--priview-->
+    <div>
+      <strong>animation-direction（默认：alternate）</strong>
+      <div class="demo_ani demo_4">
+        <div></div>
+      </div>
+    </div>
+    <!--priview-->
+    <!--code-->
+    <div style="width:800px;">
+      <script
+        type="text/plain"
+        class="language-markup"
+      >
+        <div class="demo_ani demo_4">
+            <div></div>
+        </div>
+      </script>
+
+      <pre><code class="language-scss">
+        @keyframes move{
+		  0%{left:0}
+		  100%{left:600px;}
+		}
+
+		.demo_ani{
+			position: relative;
+			height: 120px;
+		}
+
+		.demo_ani div{
+			width: 100px;
+			height: 100px;
+			border:1px solid #000;
+  			background:red;
+  			position: absolute;
+			top:0;
+			left:0;
+		}
+
+		.demo_4 div.move{
+			animation-name:move;
+			animation-duration:2s;/*单次走2s*/
+			animation-timing-function:linear;
+			animation-iteration-count:5;/*走5次，到端就算一次*/
+			animation-fill-mode:forwards;
+			animation-direction:alternate;/*连续性*/
+			/*动画播放在第偶数次向前播放，第奇数次向反方向播放,默认第一次不算*/
+		}
+      </code></pre>
+
+    </div>
+    <!--code-->
+    <br><br>
+    <button id="resetBtn">重置所有</button>
+
+  </div>
+</template>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+@keyframes move {
+  0% {
+    left: 0;
+  }
+  100% {
+    left: 600px;
+  }
+}
+
+.demo_ani {
+  position: relative;
+  height: 120px;
+
+  div {
+    width: 100px;
+    height: 100px;
+    border: 1px solid #000;
+    background: red;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+}
+
+.demo_1 div.move {
+  animation: move 2s;
+}
+
+.demo_2 div.move {
+  animation: move 2s forwards;
+}
+
+.demo_3 div.move {
+  animation: move 2s forwards 6; /*0-600px 6次*/
+}
+
+.demo_4 div.move {
+  animation-name: move;
+  animation-duration: 2s; /*单次走2s*/
+  animation-timing-function: linear;
+  animation-iteration-count: 5; /*走5次，到端就算一次*/
+  animation-fill-mode: forwards;
+  animation-direction: alternate; /*连续性*/
+  /*动画播放在第偶数次向前播放，第奇数次向反方向播放,默认第一次不算*/
+}
+</style>
+<script>
+import Prism from "prismjs";
+
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    Prism.highlightAll();
+    this.bindEvent();
+  },
+  methods: {
+    bindEvent() {
+      $(".demo_ani").on("click", function() {
+        $(this)
+          .find("div")
+          .addClass("move");
+      });
+
+      $("#resetBtn").on("click", function() {
+        $.each($(".demo_ani"), function(index, el) {
+          $(el)
+            .find("div")
+            .removeClass("move");
+        });
+      });
+    }
+  }
+};
+</script>
+
---
+++ src/views/regular/base.vue
@@ -0,0 +1,43 @@
+<template >
+  <div style="padding:30px;">
+    <!--priview-->
+    <div>
+      <strong>常用正则</strong>
+      <div>匹配数字：<span>/^[0-9]*$/</span></div>
+      <div>匹配手机号码：<span>/^1[34578]\d{9}$/</span></div>
+      <div>匹配电话号码：<span>/[0-9]{5,}$/</span></div>
+      <div>匹配座机：<span>/^\d{3,4}-\d{7,8}(-\d{3,4})?$/</span></div>
+      <div>匹配银行账号：<span>/^\d{16,19}$/</span></div>
+      <div>匹配邮箱：<span>/\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/</span></div>
+      <div>匹配网址：<span>/(([-a-zA-Z0-9:%._/\+~#=\u4e00-\u9fa5]{1,256}\.[a-zA-Z\u4e00-\u9fa5]{2,}([-a-zA-Z0-9@:%_\+.~#?&//=]*))|(((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])))/</span></div>
+      <div>匹配QQ：<span>/^[1-9]\d{3,10}$/</span></div>
+      <div>匹配文件后缀：<span>/(.jpg|.gif)+(\?|\#|$)/</span></div>
+      <div>匹配中文：<span>/^[\u4e00-\u9fa5]{0,}$/</span></div>
+      <div>匹配身份证号：<span>/(^\d{15}$)|(^\d{17}([0-9]|X)$)/</span></div>
+      <div>匹配IP地址：<span>/^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/</span></div>
+      <div>匹配首尾空格：<span>/(^\s*)|(\s*$)/g</span></div>
+    </div>
+    <!--priview-->
+
+  </div>
+</template>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+</style>
+<script>
+import Prism from "prismjs";
+
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    Prism.highlightAll();
+    this.bindEvent();
+  },
+  methods: {
+    bindEvent() {}
+  }
+};
+</script>
+
